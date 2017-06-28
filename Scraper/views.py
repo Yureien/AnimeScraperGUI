6daf330e4c18ff29e3120854d4d7b745ed62f452
FadedCoder <sohamsen1000@gmail.com>
2017-06-28 17:09:15 +0530

    Added search

--- Scraper/views.py
+++ Scraper/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect  # HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+
+import sys
+sys.path.append("/home/soham/Documents/AnimeScraperGUI/Scraper/scrapers")
+sys.path.append("/home/soham/Documents/AnimeScraperGUI/Scraper/scrapers/scrapers")
+from scraper_handler import scraper_handler
 
 
 def index(request):
@@ -13,7 +18,27 @@ def search_page(request):
 
 
 def search(request, search_txt):
-    return HttpResponse("Hello World. You searched %s" % search_txt)
+    search_types = list()
+    if request.GET:
+        if 'i' in request.GET:
+            search_types.append("Info Search")
+        if 'd' in request.GET:
+            search_types.append("Download Search")
+    else:
+        search_types.append("Info Search")
+        search_types.append("Download Search")
+    d_results_unformatted = scraper_handler.search(search_txt)
+    d_results = list()
+    i_results = list()
+    for a in d_results_unformatted:
+        for b in a:
+            d_results.append(b)
+    return render(request, "Scraper/search_results.html", {
+        "search_text": search_txt,
+        "i_results": i_results,
+        "d_results": d_results,
+        "search_types": search_types,
+    })
 
 
 def login_user(request):
