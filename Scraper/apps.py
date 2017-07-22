42c09771e1969467572074ebb0fc5eb08848987e
FadedCoder <sohamsen1000@gmail.com>
2017-07-22 18:18:28 +0530

    Bug fixes

--- Scraper/apps.py
+++ Scraper/apps.py
@@ -11,9 +11,10 @@ class ScraperConfig(AppConfig):
 
     def ready(self):
         download_path = settings.DOWNLOAD_PATH
-        cur_dir = os.getcwd()
-        os.chdir(download_path)
-        for f in glob.glob("*.tmp"):
-            logging.debug("Removing file: %s" % (f))
-            os.remove(f)
-        os.chdir(cur_dir)
+        if os.path.isdir(download_path):
+            cur_dir = os.getcwd()
+            os.chdir(download_path)
+            for f in glob.glob("*.tmp"):
+                logging.debug("Removing file: %s" % (f))
+                os.remove(f)
+            os.chdir(cur_dir)
